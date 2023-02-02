fb1e93437f5b6256143e85508d77aa1c7caaa8e8
Lionnelle ZONGO MBOM <lionnelle14@outlook.fr>
2023-02-02 15:58:36 +0100

    Commit deploy

---
+++ src/app/etagere/etagere.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tc-etagere',
+  templateUrl: './etagere.component.html',
+  styleUrls: ['./etagere.component.scss']
+})
+export class EtagereComponent {
+  isCollapsed = true;
+
+}
